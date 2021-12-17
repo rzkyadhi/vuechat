54d4d14a71969a63e493fde15d2564506bb72a85
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-12-17 18:49:55 +0700

    Mempercantik Login dan Register

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/app.css'
+
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="form-auth">
     <div v-if="showLogin">
       <Login />
       <p>
