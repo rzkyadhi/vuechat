4a5682b16db198718049c322aa465bbafe473bf6
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-12-29 15:03:32 +0700

    Mempercantik Component Chatbox

--- src/components/chat/Chatbox.vue
+++ src/components/chat/Chatbox.vue
@@ -9,6 +9,66 @@
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           </div>
       </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
+
+      <div class="chat-bubble">
+          <div class="chat-head">
+              <div>Admin</div>
+              <div>3 min ago</div>
+          </div>
+          <div class="chat-body">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </div>
+      </div>
   </div>
 </template>
 
