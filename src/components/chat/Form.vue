ef50368fe6a485db9cfb2eb16830ff5c3616162d
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2021-12-29 15:11:26 +0700

    Mempercantik Component Chat Form menggunakan Font-Awesome

--- src/components/chat/Form.vue
+++ src/components/chat/Form.vue
@@ -1,7 +1,9 @@
 <template>
-  <form action="" class="d-flex">
+  <form action="" class="d-flex chat-form">
       <input type="text" class="form-control rounded-3" placeholder="Type your message">
-      <button class="btn btn-primary rounded-3">Send</button>
+      <button class="btn btn-primary rounded-3">
+        <i class="fas fa-paper-plane"></i>
+      </button>
   </form>
 </template>
 
