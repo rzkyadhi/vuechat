5cbe72d428771046be8b85927578f2d0458f3cf9
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-01-04 15:54:58 +0700

    Menampilkan Data Chat Menggunakan GetCollection

--- src/components/chat/Chatbox.vue
+++ src/components/chat/Chatbox.vue
@@ -1,80 +1,31 @@
 <template>
-  <div class="chat-box">
-      <div class="chat-bubble">
+  <div class="chat-box" v-if="documents" ref="chat">  
+      <div class="chat-bubble" v-for="doc in documents" :key="doc.id">
           <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
+              <div>{{ doc.name }}</div>
+              <div>{{ doc.createdAt }}</div>
           </div>
           <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </div>
-      </div>
-
-      <div class="chat-bubble">
-          <div class="chat-head">
-              <div>Admin</div>
-              <div>3 min ago</div>
-          </div>
-          <div class="chat-body">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              {{ doc.message }}
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import getCollection from "../../composable/getCollection"
+import { onUpdated, ref } from "vue"
 export default {
+    setup() {
+        const { error, documents } = getCollection("message")
+        const chat = ref(null)
 
+        onUpdated(() => {
+            chat.value.scrollTop = chat.value.scrollHeight
+        })
+
+        return { error, documents, chat }
+    }
 }
 </script>
 
