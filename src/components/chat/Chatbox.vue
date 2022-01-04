8430978d853490bb6f5f43e18fcc9e583879c414
rzkyadhi <rizky.nugroho@students.amikom.ac.id>
2022-01-04 16:08:15 +0700

    Mengubah Format Timestamp lebih mudah Dibaca

--- src/components/chat/Chatbox.vue
+++ src/components/chat/Chatbox.vue
@@ -1,34 +1,41 @@
 <template>
-  <div class="chat-box" v-if="documents" ref="chat">  
-      <div class="chat-bubble" v-for="doc in documents" :key="doc.id">
-          <div class="chat-head">
-              <div>{{ doc.name }}</div>
-              <div>{{ doc.createdAt }}</div>
-          </div>
-          <div class="chat-body">
-              {{ doc.message }}
-          </div>
+  <div v-if="documents" ref="chat" class="chat-box">
+    <div v-for="doc in formattedDocuments" :key="doc.id" class="chat-bubble">
+      <div class="chat-head">
+        <div>{{ doc.name }}</div>
+        <div>{{ doc.createdAt }}</div>
       </div>
+      <div class="chat-body">
+        {{ doc.message }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import getCollection from "../../composable/getCollection"
-import { onUpdated, ref } from "vue"
+import getCollection from '../../composable/getCollection'
+import { computed, onUpdated, ref } from 'vue'
+import { formatDistanceToNow } from 'date-fns'
+
 export default {
-    setup() {
-        const { error, documents } = getCollection("message")
-        const chat = ref(null)
+  setup() {
+    const { error, documents } = getCollection('message')
+    const chat = ref(null)
 
-        onUpdated(() => {
-            chat.value.scrollTop = chat.value.scrollHeight
-        })
-
-        return { error, documents, chat }
-    }
+    const formattedDocuments = computed(() => {
+        if (documents.value) {
+            return documents.value.map((doc) => {
+                let time = formatDistanceToNow(doc.createdAt.toDate())
+                return { ...doc, createdAt: time}
+            })
+        }
+    })
+    onUpdated(() => {
+      chat.value.scrollTop = chat.value.scrollHeight
+    })
+    return { error, documents, chat, formattedDocuments }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
